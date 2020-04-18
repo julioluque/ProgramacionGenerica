1946d2c1a59dd8f5d4810fb6b6024a3b8e18c0ab
Julio Luque <julio.a.luque@gmail.com>
2020-04-18 20:11:49 -0300

    Cambiamos array por arrayList

--- src/ArrayList/UsoEmpleado.java
+++ src/ArrayList/UsoEmpleado.java
@@ -5,10 +5,27 @@ import java.util.Iterator;
 
 public class UsoEmpleado {
 	public static void main(String[] args) {
-		Empleado listaEmpleados[] = new Empleado[3];
-		listaEmpleados[0] = new Empleado("Juan",45,2500);
-		listaEmpleados[1] = new Empleado("Ana",55,2000);
-		listaEmpleados[2] = new Empleado("Luis",25,2600);
+
+		ArrayList<Empleado> listaEmpleados = new ArrayList<Empleado>();
+		listaEmpleados.ensureCapacity(16);
+		
+		listaEmpleados.add(new Empleado("Ana",45,2500));
+		listaEmpleados.add(new Empleado("Braulio",55,2000));
+		listaEmpleados.add(new Empleado("Carlos",25,2600));
+		listaEmpleados.add(new Empleado("David",25,2600));
+		listaEmpleados.add(new Empleado("Esteban",25,2600));
+		listaEmpleados.add(new Empleado("Felipe",45,2500));
+		listaEmpleados.add(new Empleado("Gustavo",55,2000));
+		listaEmpleados.add(new Empleado("Herminia",25,2600));
+		listaEmpleados.add(new Empleado("Ines",25,2600));
+		listaEmpleados.add(new Empleado("Julia",25,2600));
+		listaEmpleados.add(new Empleado("Karina",45,2500));
+		listaEmpleados.add(new Empleado("Luis",55,2000));
+		listaEmpleados.add(new Empleado("Matias",25,2600));
+		listaEmpleados.add(new Empleado("Noelia",25,2600));
+		listaEmpleados.add(new Empleado("Omar",25,2600));
+		
+		System.out.println(listaEmpleados.size());
 			
 		for(Empleado e:listaEmpleados) {
 			System.out.println(e.dameDatos());
